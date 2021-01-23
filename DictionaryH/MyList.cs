6f42fee8d4116a5b25790e063c0865f6fa543a7f
nursebil canbolat <nursebiltw@gmail.com>
2021-01-23 20:50:36 +0300

    Dictionary added.

---
+++ DictionaryH/MyList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictionaryH
+{
+    class MyList<T>
+    {
+        T[] books;
+        T[] tempBooks;
+
+        //constructor
+        public MyList()
+        {
+            books = new T[0];
+        }
+
+        public void Add(T item)
+        {
+            tempBooks = books;
+            books = new T[books.Length + 1];
+
+            for (int i = 0; i < tempBooks.Length; i++)
+            {
+                books[i] = tempBooks[i];
+            }
+
+            books[books.Length - 1] = item;
+
+            //Console.WriteLine($"{item} added!");
+        }
+
+    }
+}
---
+++ DictionaryH/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DictionaryH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> bookNames = new MyList<string>();
+            bookNames.Add("1984");
+            bookNames.Add("Hunger Games-Mockingjay");
+
+            
+        }
+    }
+}
